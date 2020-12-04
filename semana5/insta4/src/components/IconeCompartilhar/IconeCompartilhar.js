80a11adb1c61accc0c55dec81b0cd38d0994d534
crisrocha025 <crisfabricio0104@gmail.com>
2020-12-04 19:12:19 -0200

    Projeto pronto, sem desafios

--- semana5/insta4/src/components/IconeCompartilhar/IconeCompartilhar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './IconeCompartilhar.css'
-
-export function IconeCompartilhar(props) {
-	return <div className={'icon-container'}>
-		<img alt={'Icone'} src={props.icone} onClick={props.onClickIcone}/>
-	</div>
-}
